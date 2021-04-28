eb143fc44cfea59b0ce5036c1deacf6aed792e96
Marco Ferreira <marcoavfcc@gmail.com>
2021-04-28 10:09:01 -0300

    atualizar

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import './styles/global.scss';
 import './styles/sidebar.scss';
 import './styles/content.scss';
 import { Content } from './components/Content';
+import { SideBar } from './components/SideBar';
 
 interface GenreResponseProps {
   id: number;
@@ -61,40 +62,11 @@ export function App() {
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <nav className="sidebar">
-        <span>Watch<p>Me</p></span>
-
-        <div className="buttons-container">
-          {genres.map(genre => (
-            <Button
-              key={String(genre.id)}
-              title={genre.title}
-              iconName={genre.name}
-              onClick={() => handleClickButton(genre.id)}
-              selected={selectedGenreId === genre.id}
-            />
-          ))}
-        </div>
-
-      </nav>
-
+    
+      <SideBar genero={genres} funcao={handleClickButton} id={selectedGenreId} />
       <Content select={selectedGenre} movie={movies} />
       
      
     </div>
   )
 }
-
- {/*<div className="container">
-  <header>
-    <span className="category">Categoria:<span>< {selectedGenre.title}></span></span>
-  </header>
-
-  <main>
-    <div className="movies-list">
-      {movies.map(movie => (
-        <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-      ))}
-    </div>
-  </main>
-</div>*/}
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,36 @@
-export function SideBar() {
+import '../styles/sidebar.scss'
+import { Button } from './Button'
+
+interface Genero {
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;  
+}
+
+interface SidebarProps {
+  genero: Genero[];
+  funcao: (id: number) => void;
+  id: number;
+}
+
+export function SideBar({ funcao, genero, id }: SidebarProps) {
   // Complete aqui
+  return(
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genero.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => funcao(genre.id)}
+              selected={id === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+  )
 }
